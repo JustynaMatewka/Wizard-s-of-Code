3b1ca2aed2f3ea22e22f58c03b89debd4dbce020
Adam Szweda <maxsuspl@gmail.com>
2023-11-04 11:54:24 +0100

    binary tree map and TreeDraw function

--- map-generator.js
+++ map-generator.js
@@ -1,15 +1,15 @@
-var canvas = document.getElementById("canvas1");
-var mojObiekt = canvas.getContext('2d');
-mojObiekt.fillStyle = "#E44D26";
-mojObiekt.fillRect(10,10,190,750);
+var canvas = document.getElementById('okno');
+var ctx = canvas.getContext('2d');
 
 class Node {
     constructor(data) {
       this.data = data;
       this.left = null;
       this.right = null;
+      this.x = 0; // Współrzędna x do rysowania
+      this.y = 0; // Współrzędna y do rysowania
     }
-  }
+}
   
 class BinaryTree {
     constructor() {
@@ -58,15 +58,88 @@ class BinaryTree {
         return true;
         }
     }
+    draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (this.root) {
+          this.setNodeCoordinates(this.root, canvas.width / 2, 50, canvas.width / 4);
+          this.drawNode(this.root);
+        }
+      }
+    setNodeCoordinates(node, x, y, horizontalOffset) {
+        if (node) {
+            node.x = x;
+            node.y = y;
+            this.setNodeCoordinates(node.left, x - horizontalOffset, y + 80, horizontalOffset / 2);
+            this.setNodeCoordinates(node.right, x + horizontalOffset, y + 80, horizontalOffset / 2);
+        }
+    }
+    drawNode(node) {
+        if (node) {
+          ctx.beginPath();
+          ctx.arc(node.x, node.y, 20, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fillText(node.data, node.x - 5, node.y + 5);
+          ctx.closePath();
+
+          if (node.left) {
+            ctx.beginPath();
+            ctx.moveTo(node.x, node.y);
+            ctx.lineTo(node.left.x, node.left.y);
+            ctx.stroke();
+            ctx.closePath();
+            this.drawNode(node.left);
+          }
+          if (node.right) {
+            ctx.beginPath();
+            ctx.moveTo(node.x, node.y);
+            ctx.lineTo(node.right.x, node.right.y);
+            ctx.stroke();
+            ctx.closePath();
+            this.drawNode(node.right);
+          }
+        }
+    }
 }
-  
-  const binaryTree = new BinaryTree();
-  binaryTree.insert(10);
-  binaryTree.insert(5);
-  binaryTree.insert(15);
-  binaryTree.insert(3);
-  binaryTree.insert(7);
-  
-  console.log(binaryTree.search(5)); // true
-  console.log(binaryTree.search(8)); // false
-  
+
+class roomFight {
+    constructor(){
+
+    }
+}
+
+class roomRest {
+    constructor(){
+
+    }
+}
+
+class roomTherapy {
+    constructor(){
+
+    }
+}
+
+class roomShop {
+    constructor(){
+
+    }
+}
+
+class roomJobInterview {
+    constructor(){
+
+    }
+}
+
+function generateMap() {
+    const binaryTree = new BinaryTree();
+
+    binaryTree.insert(10);
+    binaryTree.insert(5);
+    binaryTree.insert(15);
+    binaryTree.insert(3);
+    binaryTree.insert(7);  
+    binaryTree.draw();
+}
+
+generateMap();
